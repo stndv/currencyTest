0f6f8cdf890b0e06cf097beadaf72e67c43588d9
stndv <stndvbakyt@gmail.com>
2021-11-02 17:34:30 +0600

    insert DATA into DB

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ if (!$link) {
     die('Не удалось соединиться : ' . mysql_error());
 }
 
-// выбираем foo в качестве текущей базы данных
+// выбираем currencytest в качестве текущей базы данных
 $db_selected = mysqli_select_db( $link, 'currencytest');
 if (!$db_selected) {
-    die ('Не удалось выбрать базу foo: ' . mysql_error());
+    die ('Не удалось выбрать базу currencytest: ' . mysql_error());
 }
--- shell.php
+++ shell.php
@@ -3,29 +3,55 @@ require_once('config.php');
 
 $filename = 'http://www.cbr.ru/scripts/XML_daily.asp';
 
-$data = simplexml_load_file($filename);
+$data = simplexml_load_file($filename);//load data into a variable
 
-$value = $data->Valute[0];
-
-    $index = 0;
-    $length = count($data);
+    $length = count($data);//length of objects in xml
+    
     var_dump($length);
+
+   $index = 0;
+  
     while($index < $length)
     {
+      
         $value = $data->Valute[$index];
         
         $values_array=(array)$value;
+        //ID
+        $id = $values_array["@attributes"]['ID'];
         // AMD RUB USD BRL BGN
         $name = $values_array['CharCode'];
         // ammount of money
         $rate=$values_array['Value'];
-        //ID
-        $id = $values_array["@attributes"]['ID'];
-
+    
+        $frate = str_replace(',','.',$rate);
         var_dump($name);
-        var_dump($rate);
+        var_dump($frate);
         var_dump($id);
+
+        $sql = "INSERT INTO currency (`id`, `name`, `rate`) VALUES ( '$id', '$name', '$frate')";
+
+        if (mysqli_query($link, $sql)) {
+            echo "Успешно создана новая запись";
+        } else {
+            echo "Ошибка: " . $sql . "<br>" . mysqli_error($link);
+        }
 
         $index++;
 
     }
+    
+    
+    /*
+    // sql to delete a record
+    $sql = "DELETE FROM currency WHERE id=123";
+
+    if ($link->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+    } else {
+    echo "Error deleting record: " . $conn->error;
+    }
+
+
+    //$value = $data->Valute[0];//choose a specific object to show
+*/
