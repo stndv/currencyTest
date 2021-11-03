58033e121adb90ed995a35e0221378a1ac77c440
Anton Kritsky <ant.kritsky@gmail.com>
2021-11-03 15:49:22 +0600

    GET currencies list with pagination example

--- index.php
+++ index.php
@@ -13,21 +13,24 @@ $currencies = [];
 while ($row = mysqli_fetch_array($query)) {
     $currencies[] = $row;
 }
-
+$total = 35;
+$pagesTotal = $total / $limit;
 $data = [
     '_links' => [
-        'self' => '...',
-        'first' => '...',
-        'prev' => '...',
-        'next' => '...',
-        'last' => '...',
+        'self' => $page ? '/page=' . $page : '/',
+        'first' => '/',
+        'prev' => $page > 1 ? '/page=' . ($page - 1) : '',
+        'next' => $page > 0 && $page < $pagesTotal ? '/page=' . ($page + 1) : '',
+        'last' => $pagesTotal > 1  ? '/page=' . $pagesTotal : '/',
     ],
 
     'count' => 3,
     'total' => 498,
-    '_embedded'
+    '_embedded' => [
+        'currencies' => []
+    ]
 ];
-$data['_embedded'] = $currencies;
+$data['_embedded']['currencies'] = $currencies;
 
-echo json_encode($data);
+echo json_encode($data, JSON_PRETTY_PRINT);
 
